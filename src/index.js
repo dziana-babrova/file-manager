36e911ca723a83511b825a7641dac2a02e0b1c63
Diana_Bobrova <dziana_babrova@epam.com>
2023-06-18 19:35:29 +0300

    feat: add current directory displaying

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 import { createInterface } from 'node:readline';
-import { stdin as input, stdout as output, argv } from 'process';
+import { stdin as input, stdout as output, argv, cwd } from 'process';
 import { displayGreeting } from './general/greeting.js';
 import { getUsername } from './general/username.js';
 import { parseInput } from './input/parseInput.js';
 import { displayLeave } from './general/leave.js';
+import { displayCurrentDirectory } from './navigation/currentDirectory.js';
 
 const App = () => {
   try {
     let name = getUsername(argv.slice(2));
     displayGreeting(name);
+    displayCurrentDirectory();
+
     const rl = createInterface({ input, output });
 
     rl.on('line', (input) => {
       try {
         if (input === '.exit') rl.close()
-        else parseInput(input);
+        else {
+          parseInput(input);
+        };
       } catch (e) {
         console.error(e.message);
+      } finally {
+        displayCurrentDirectory();
       }
     })
     rl.on('close', () => {
