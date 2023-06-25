49eb5ef531a573e17bea26af58d5b54b87f4228a
Diana_Bobrova <dziana_babrova@epam.com>
2023-06-25 16:02:53 +0300

    feat: replace erro throwing with default value if no name

--- src/general/username.js
+++ src/general/username.js
@@ -4,7 +4,7 @@ const getUsername = (argv) => {
     const paramName = '--username=';
     if (param.startsWith(paramName)) return param.replace(paramName, '');
   } catch {
-    throw new Error('Username is not provided properly. Check README.md to get the proper command to start the app');
+    return 'Anonymous'
   }
 };
 
