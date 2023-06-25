d7fd5a60f25a4c732f0caad9e9391bfa4fa5dbcb
Diana_Bobrova <dziana_babrova@epam.com>
2023-06-25 16:00:13 +0300

    fix: start with homedir

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import { createInterface } from 'node:readline';
-import { stdin as input, stdout as output, argv, cwd } from 'process';
+import { stdin as input, stdout as output, argv } from 'process';
 import { getUsername } from './general/username.js';
 import { parseInput } from './input/parseInput.js';
 import { displayCurrentDirectory } from './navigation/navigation.js';
+import { chdir } from 'node:process';
+import { homedir } from 'node:os';
 
 const App = async () => {
   const rl = createInterface({ input, output });
   try {
     let name = getUsername(argv.slice(2));
     output.write(`Welcome to the File Manager, ${name}!\n`);
+    chdir(homedir());
     displayCurrentDirectory();
 
     rl.on('line', async (input) => {
